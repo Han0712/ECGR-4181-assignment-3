24a1fec93bc55b9bf552223980f9440437e86f27
Han0712 <90944528+Han0712@users.noreply.github.com>
2022-10-05 14:26:32 -0400

    Update decoder.cpp

--- decoder.cpp
+++ decoder.cpp
@@ -31,109 +31,109 @@ string print_opcode_type(opcode_type) {
 
 void getOpcode(inst) {
     uint8_t opcode;
-    n = (uint8_t)((inst & 0b00000000000000000000000001111111) >> 0);
+    opcode = (uint8_t)((inst & 0b00000000000000000000000001111111) >> 0);
     return opcode;
 }
 
 void getOpcode(inst) {
     uint8_t rd;
-    n = (uint8_t)((inst & 0b00000000000000000000111110000000) >> 7);
+    rd = (uint8_t)((inst & 0b00000000000000000000111110000000) >> 7);
     return rd;
 }
 
 void getOpcode(inst) {
     uint8_t fun3;
-    n = (uint8_t)((inst & 0b00000000000000000111000000000000) >> 12);
+    fun3 = (uint8_t)((inst & 0b00000000000000000111000000000000) >> 12);
     return rs1;
 }
 
 void getOpcode(inst) {
     uint8_t rs1;
-    n = (uint8_t)((inst & 0b00000000000011111000000000000000) >> 15);
+    rs1 = (uint8_t)((inst & 0b00000000000011111000000000000000) >> 15);
     return rs1;
 }
 
 void getOpcode(inst) {
     uint8_t rs2;
-    n = (uint8_t)((inst & 0b00000001111100000000000000000000) >> 20);
+    rs2 = (uint8_t)((inst & 0b00000001111100000000000000000000) >> 20);
     return opcode;
 }
 
 void getOpcode(inst) {
     uint8_t fun7;
-    n = (uint8_t)((inst & 0b11111110000000000000000000000000) >> 25);
+    fun7 = (uint8_t)((inst & 0b11111110000000000000000000000000) >> 25);
     return fun7;
 }
 
 void getOpcode(inst) {
     uint8_t imm11_0;
-    n = (uint8_t)((inst & 0b11111111111100000000000000000000) >> 20);
+    imm11_0 = (uint8_t)((inst & 0b11111111111100000000000000000000) >> 20);
     return imm11_0;
 }
 
 void getOpcode(inst) {
     uint8_t imm4_0;
-    n = (uint8_t)((inst & 0b00000000000000000000111110000000) >> 7);
+    imm4_0 = (uint8_t)((inst & 0b00000000000000000000111110000000) >> 7);
     return imm4_0;
 }
 
 void getOpcode(inst) {
     uint8_t imm11_5;
-    n = (uint8_t)((inst & 0b11111110000000000000000000000000) >> 25);
+    imm11_5 = (uint8_t)((inst & 0b11111110000000000000000000000000) >> 25);
     return imm11_5;
 }
 
 void getOpcode(inst) {
     uint8_t imm11_B;
-    n = (uint8_t)((inst & 0b00000000000000000000000010000000) >> 7);
+    imm11_B = (uint8_t)((inst & 0b00000000000000000000000010000000) >> 7);
     return imm11_B;
 }
 
 void getOpcode(inst) {
     uint8_t imm4_1;
-    n = (uint8_t)((inst & 0b00000000000000000000111100000000) >> 8);
+    imm4_1 = (uint8_t)((inst & 0b00000000000000000000111100000000) >> 8);
     return imm4_1;
 }
 
 void getOpcode(inst) {
     uint8_t imm10_5;
-    n = (uint8_t)((inst & 0b00111110000000000000000000000000) >> 25);
+    imm10_5 = (uint8_t)((inst & 0b00111110000000000000000000000000) >> 25);
     return imm10_5;
 }
 
 void getOpcode(inst) {
     uint8_t imm12;
-    n = (uint8_t)((inst & 0b11000000000000000000000000000000) >> 30);
+    imm12 = (uint8_t)((inst & 0b11000000000000000000000000000000) >> 30);
     return imm12;
 }
 
 void getOpcode(inst) {
     uint8_t imm31_12;
-    n = (uint8_t)((inst & 0b11111111111111111111000000000000) >> 12);
+    imm31_12 = (uint8_t)((inst & 0b11111111111111111111000000000000) >> 12);
     return imm31_12;
 }
 
 void getOpcode(inst) {
     uint8_t imm19_12;
-    n = (uint8_t)((inst & 0b00000000000011111111000000000000) >> 12);
+    imm19_12 = (uint8_t)((inst & 0b00000000000011111111000000000000) >> 12);
     return imm19_12;
 }
 
 void getOpcode(inst) {
     uint8_t imm11_J;
-    n = (uint8_t)((inst & 0b00000000000100000000000000000000) >> 20);
+    imm11_J = (uint8_t)((inst & 0b00000000000100000000000000000000) >> 20);
     return imm12;
 }
 
 void getOpcode(inst) {
     uint8_t imm10_1;
-    n = (uint8_t)((inst & 0b01111111111000000000000000000000) >> 21);
+    imm10_1 = (uint8_t)((inst & 0b01111111111000000000000000000000) >> 21);
     return imm10_1;
 }
 
 void getOpcode(inst) {
     uint8_t imm20;
-    n = (uint8_t)((inst & 0b10000000000000000000000000000000) >> 31);
+    imm20 = (uint8_t)((inst & 0b10000000000000000000000000000000) >> 31);
     return imm20;
 }
 
